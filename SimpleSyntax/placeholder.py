e3af02b550c1f6d08179832af73a5a6089969650
Joao Ramos <joao.ramos@uk.prime-research.local>
2019-06-23 20:39:12 +0100

    placeholders

--- SimpleSyntax/placeholder.py
+++ SimpleSyntax/placeholder.py
@@ -10,3 +10,11 @@ with tf.compat.v1.Session() as Sess:
     result=Sess.run(operacao, feed_dict={p:[1,2,3]})
     print("--->",result)
 
+
+p2=tf.compat.v1.placeholder('float', [None,5])
+operation2=p2*5
+
+with tf.compat.v1.Session() as Sess:
+    dataTMP=[[1,2,3,4,5],[6,7,8,9,10]]
+    result=Sess.run(operation2, feed_dict={p2:dataTMP})
+    print("--->",result)
