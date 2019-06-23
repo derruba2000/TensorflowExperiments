1894b08653fdeef0b0df296e24872bc9d42bf0dc
Joao Ramos <joao.ramos@uk.prime-research.local>
2019-06-23 21:08:02 +0100

    Placeholder

--- SimpleSyntax/TensorBoard.py
+++ SimpleSyntax/TensorBoard.py
@@ -2,9 +2,12 @@ import tensorflow as tf
 
 tf.reset_default_graph();
 
-a=tf.add(2,2,name='add')
-b=tf.multiply(a,3,name='mult')
-c=tf.multiply(b,a,name='mult2')
+with tf.name_scope('Operations'):
+    with tf.name_scope('Adds'):
+        a=tf.add(2,2,name='add')
+    with tf.name_scope('Mutls'):
+        b=tf.multiply(a,3,name='mult')
+        c=tf.multiply(b,a,name='mult2')
 
 
 
